e36fcdc724a4c15106d72c2b01408cea5d39ff2f
Achapeau <127884427+Achapeau@users.noreply.github.com>
2023-09-28 11:26:42 +0200

    quick fix on checkUserDoesntExists

--- backend/src/middlewares/checkUser.js
+++ backend/src/middlewares/checkUser.js
@@ -1,12 +1,11 @@
 const models = require("../models");
 
 const checkUserDoesntExists = async (req, res, next) => {
-  const [user] = await models.user.findOneByEmail(req.body.email);
+  const [user] = await models.user.findOneByEmail(req.body);
 
   if (user.length) {
     return res.status(400).json({ message: "User already exists" });
   }
-
   return next();
 };
 
